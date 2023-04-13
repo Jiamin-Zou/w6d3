a8a96d3ab82a1810c3a4d58a1b5e8235b1e08e58
Jiamin-Zou <jiaminz622@gmail.com>
2023-04-13 13:49:28 -0400

    Add index on username and add db constraint unique

--- practice-for-ch-rails-first-routes-long-practice-main/routes_and_controller/db/schema.rb
+++ practice-for-ch-rails-first-routes-long-practice-main/routes_and_controller/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_13_155345) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_13_174657) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -18,6 +18,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_13_155345) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "username", null: false
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
