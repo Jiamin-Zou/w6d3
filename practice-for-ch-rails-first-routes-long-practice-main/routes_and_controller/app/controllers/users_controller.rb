496e970ea60417bd56a1f6082ac85d29f9ce67cc
Jiamin-Zou <jiaminz622@gmail.com>
2023-04-13 11:48:29 -0400

    Completed First routes & controllers

--- practice-for-ch-rails-first-routes-long-practice-main/routes_and_controller/app/controllers/users_controller.rb
+++ practice-for-ch-rails-first-routes-long-practice-main/routes_and_controller/app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
 
     def create
         debugger
-        @user = User.new(params.require(:user).permit(:name, :email))
+        @user = User.new(user_params)
         if @user.save
           render json: @user
         else
@@ -23,6 +23,33 @@ class UsersController < ApplicationController
       @user = User.find(params[:id])
 
       render json: @user
+    end
+
+    def update
+      @user = User.find(params[:id])
+
+      if @user.update(user_params)
+        # redirect_to user_url(@user)
+        render json:@user
+      else
+        render json:@user.errors.full_messages, status: 422
+      end
+    end
+
+    def destroy
+      @user = User.find(params[:id])
+
+      if @user.destroy
+        return true
+      else
+        render json:@user.errors.full_messages, status: 422
+      end
+    end
+
+    private
+
+    def user_params
+      params.require(:user).permit(:name, :email)
     end
 
 
